e153a0e0e1ed9b84d814486f9cd5726d884fa30d
Etherx <80154345+ethrex88@users.noreply.github.com>
2021-10-21 00:36:16 +0530

    Update App.js
    scroll to smoothScroll

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
-import ScrollToTop from './components/ScrollToTop';
+import SmoothScrollbar from './components/SmoothScrollbar';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -13,7 +13,7 @@ export default function App() {
     <>
       <Router>
         <NavMenu />
-        <ScrollToTop />
+        <SmoothScroll />
         <Switch>
           <Route path="/about">
             <About />

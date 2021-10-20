522beebe5b3eb3de42db66eabd81a8c7444565d9
Etherx <80154345+ethrex88@users.noreply.github.com>
2021-10-21 00:52:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
 import ScrollToTop from './components/ScrollToTop';
-//import SmoothScrollbar from './components/SmoothScrollbar';
+
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
